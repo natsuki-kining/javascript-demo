3bafc09f8618b787d974b1f04c03ee809b26f4bf
natsuki-kining <natsuki_kining@126.com>
2020-08-04 17:48:20 +0800

    奇怪的写法

--- datav/test/index.js
+++ datav/test/index.js
@@ -11,142 +11,13 @@ var digitalFlop = Vue.extend({
       },
       methods: {
         createData () {
-          const { randomExtend } = this
-    
-          this.digitalFlopData = [
-            {
-              title: '管养里程',
-              number: {
-                number: [randomExtend(20000, 30000)],
-                content: '{nt}',
-                textAlign: 'right',
-                style: {
-                  fill: '#4d99fc',
-                  fontWeight: 'bold'
-                }
-              },
-              unit: '公里'
-            },
-            {
-              title: '桥梁',
-              number: {
-                number: [randomExtend(20, 30)],
-                content: '{nt}',
-                textAlign: 'right',
-                style: {
-                  fill: '#f46827',
-                  fontWeight: 'bold'
-                }
-              },
-              unit: '座'
-            },
-            {
-              title: '涵洞隧道',
-              number: {
-                number: [randomExtend(20, 30)],
-                content: '{nt}',
-                textAlign: 'right',
-                style: {
-                  fill: '#40faee',
-                  fontWeight: 'bold'
-                }
-              },
-              unit: '个'
-            },
-            {
-              title: '匝道',
-              number: {
-                number: [randomExtend(10, 20)],
-                content: '{nt}',
-                textAlign: 'right',
-                style: {
-                  fill: '#4d99fc',
-                  fontWeight: 'bold'
-                }
-              },
-              unit: '个'
-            },
-            {
-              title: '隧道',
-              number: {
-                number: [randomExtend(5, 10)],
-                content: '{nt}',
-                textAlign: 'right',
-                style: {
-                  fill: '#f46827',
-                  fontWeight: 'bold'
-                }
-              },
-              unit: '个'
-            },
-            {
-              title: '服务区',
-              number: {
-                number: [randomExtend(5, 10)],
-                content: '{nt}',
-                textAlign: 'right',
-                style: {
-                  fill: '#40faee',
-                  fontWeight: 'bold'
-                }
-              },
-              unit: '个'
-            },
-            {
-              title: '收费站',
-              number: {
-                number: [randomExtend(5, 10)],
-                content: '{nt}',
-                textAlign: 'right',
-                style: {
-                  fill: '#4d99fc',
-                  fontWeight: 'bold'
-                }
-              },
-              unit: '个'
-            },
-            {
-              title: '超限站',
-              number: {
-                number: [randomExtend(5, 10)],
-                content: '{nt}',
-                textAlign: 'right',
-                style: {
-                  fill: '#f46827',
-                  fontWeight: 'bold'
-                }
-              },
-              unit: '个'
-            },
-            {
-              title: '停车区',
-              number: {
-                number: [randomExtend(5, 10)],
-                content: '{nt}',
-                textAlign: 'right',
-                style: {
-                  fill: '#40faee',
-                  fontWeight: 'bold'
-                }
-              },
-              unit: '个'
-            }
-          ]
-        },
-        randomExtend (minNum, maxNum) {
-          if (arguments.length === 1) {
-            return parseInt(Math.random() * minNum + 1, 10)
-          } else {
-            return parseInt(Math.random() * (maxNum - minNum + 1) + minNum, 10)
-          }
+          this.digitalFlopData = getDigitalFlopData();
         }
       },
       mounted () {
-        const { createData } = this
-    
-        createData()
-    
-        setInterval(createData, 3000)
+        const { createData } = this;
+        createData();
+        setInterval(createData, 3000);
       }
 });
 
@@ -172,126 +43,126 @@ function randomExtend (minNum, maxNum) {
     } else {
       return parseInt(Math.random() * (maxNum - minNum + 1) + minNum, 10)
     }
-  }
+}
 
-// function getDigitalFlopData(){
-//     return [
-//         {
-//           title: '管养里程',
-//           number: {
-//             number: [randomExtend(20000, 30000)],
-//             content: '{nt}',
-//             textAlign: 'right',
-//             style: {
-//               fill: '#4d99fc',
-//               fontWeight: 'bold'
-//             }
-//           },
-//           unit: '公里'
-//         },
-//         {
-//           title: '桥梁',
-//           number: {
-//             number: [randomExtend(20, 30)],
-//             content: '{nt}',
-//             textAlign: 'right',
-//             style: {
-//               fill: '#f46827',
-//               fontWeight: 'bold'
-//             }
-//           },
-//           unit: '座'
-//         },
-//         {
-//           title: '涵洞隧道',
-//           number: {
-//             number: [randomExtend(20, 30)],
-//             content: '{nt}',
-//             textAlign: 'right',
-//             style: {
-//               fill: '#40faee',
-//               fontWeight: 'bold'
-//             }
-//           },
-//           unit: '个'
-//         },
-//         {
-//           title: '匝道',
-//           number: {
-//             number: [randomExtend(10, 20)],
-//             content: '{nt}',
-//             textAlign: 'right',
-//             style: {
-//               fill: '#4d99fc',
-//               fontWeight: 'bold'
-//             }
-//           },
-//           unit: '个'
-//         },
-//         {
-//           title: '隧道',
-//           number: {
-//             number: [randomExtend(5, 10)],
-//             content: '{nt}',
-//             textAlign: 'right',
-//             style: {
-//               fill: '#f46827',
-//               fontWeight: 'bold'
-//             }
-//           },
-//           unit: '个'
-//         },
-//         {
-//           title: '服务区',
-//           number: {
-//             number: [randomExtend(5, 10)],
-//             content: '{nt}',
-//             textAlign: 'right',
-//             style: {
-//               fill: '#40faee',
-//               fontWeight: 'bold'
-//             }
-//           },
-//           unit: '个'
-//         },
-//         {
-//           title: '收费站',
-//           number: {
-//             number: [randomExtend(5, 10)],
-//             content: '{nt}',
-//             textAlign: 'right',
-//             style: {
-//               fill: '#4d99fc',
-//               fontWeight: 'bold'
-//             }
-//           },
-//           unit: '个'
-//         },
-//         {
-//           title: '超限站',
-//           number: {
-//             number: [randomExtend(5, 10)],
-//             content: '{nt}',
-//             textAlign: 'right',
-//             style: {
-//               fill: '#f46827',
-//               fontWeight: 'bold'
-//             }
-//           },
-//           unit: '个'
-//         },
-//         {
-//           title: '停车区',
-//           number: {
-//             number: [randomExtend(5, 10)],
-//             content: '{nt}',
-//             textAlign: 'right',
-//             style: {
-//               fill: '#40faee',
-//               fontWeight: 'bold'
-//             }
-//           },
-//           unit: '个'
-//         }
-//       ]
-// }
+function getDigitalFlopData(){
+    return [
+        {
+          title: '管养里程',
+          number: {
+            number: [randomExtend(20000, 30000)],
+            content: '{nt}',
+            textAlign: 'right',
+            style: {
+              fill: '#4d99fc',
+              fontWeight: 'bold'
+            }
+          },
+          unit: '公里'
+        },
+        {
+          title: '桥梁',
+          number: {
+            number: [randomExtend(20, 30)],
+            content: '{nt}',
+            textAlign: 'right',
+            style: {
+              fill: '#f46827',
+              fontWeight: 'bold'
+            }
+          },
+          unit: '座'
+        },
+        {
+          title: '涵洞隧道',
+          number: {
+            number: [randomExtend(20, 30)],
+            content: '{nt}',
+            textAlign: 'right',
+            style: {
+              fill: '#40faee',
+              fontWeight: 'bold'
+            }
+          },
+          unit: '个'
+        },
+        {
+          title: '匝道',
+          number: {
+            number: [randomExtend(10, 20)],
+            content: '{nt}',
+            textAlign: 'right',
+            style: {
+              fill: '#4d99fc',
+              fontWeight: 'bold'
+            }
+          },
+          unit: '个'
+        },
+        {
+          title: '隧道',
+          number: {
+            number: [randomExtend(5, 10)],
+            content: '{nt}',
+            textAlign: 'right',
+            style: {
+              fill: '#f46827',
+              fontWeight: 'bold'
+            }
+          },
+          unit: '个'
+        },
+        {
+          title: '服务区',
+          number: {
+            number: [randomExtend(5, 10)],
+            content: '{nt}',
+            textAlign: 'right',
+            style: {
+              fill: '#40faee',
+              fontWeight: 'bold'
+            }
+          },
+          unit: '个'
+        },
+        {
+          title: '收费站',
+          number: {
+            number: [randomExtend(5, 10)],
+            content: '{nt}',
+            textAlign: 'right',
+            style: {
+              fill: '#4d99fc',
+              fontWeight: 'bold'
+            }
+          },
+          unit: '个'
+        },
+        {
+          title: '超限站',
+          number: {
+            number: [randomExtend(5, 10)],
+            content: '{nt}',
+            textAlign: 'right',
+            style: {
+              fill: '#f46827',
+              fontWeight: 'bold'
+            }
+          },
+          unit: '个'
+        },
+        {
+          title: '停车区',
+          number: {
+            number: [randomExtend(5, 10)],
+            content: '{nt}',
+            textAlign: 'right',
+            style: {
+              fill: '#40faee',
+              fontWeight: 'bold'
+            }
+          },
+          unit: '个'
+        }
+      ]
+}
